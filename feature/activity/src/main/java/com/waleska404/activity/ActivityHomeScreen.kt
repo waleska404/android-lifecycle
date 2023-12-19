6eb4ecb98d31daa6b79ba6df88276f235982e7e4
Paula Boyano Ivars <paulaboyanoivars@gmail.com>
2023-12-19 21:08:58 +0100

    :lipstick: fix home activity screen style

--- feature/activity/src/main/java/com/waleska404/activity/ActivityHomeScreen.kt
+++ feature/activity/src/main/java/com/waleska404/activity/ActivityHomeScreen.kt
@@ -5,7 +5,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,19 +15,25 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.waleska404.activity.create.FirstActivity
 
 @Composable
 fun ActivityHomeScreen() {
     val context = LocalContext.current
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxSize().padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top
     ) {
+        Spacer(modifier = Modifier.height(12.dp))
+        Text(
+            fontSize = 30.sp,
+            text = "Activity Topics"
+        )
         Spacer(modifier = Modifier.height(22.dp))
-        Text(text = "Activity Topics")
         Button(
+            modifier = Modifier.fillMaxWidth(),
             onClick = {
                 context.startActivity(
                     Intent(context, FirstActivity::class.java)
@@ -33,6 +41,22 @@ fun ActivityHomeScreen() {
             }
         ) {
             Text(text = "Create and Destroy")
+        }
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = {
+
+            }
+        ) {
+            Text(text = "Start and Stop")
+        }
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = {
+
+            }
+        ) {
+            Text(text = "Resume and Pause")
         }
     }
 }
