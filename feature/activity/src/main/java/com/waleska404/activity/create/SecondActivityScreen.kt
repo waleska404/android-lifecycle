8b1a519cb42e16049a1fab403313262e3993f6f5
Paula Boyano Ivars <paulaboyanoivars@gmail.com>
2023-12-19 21:23:47 +0100

    :lipstick: fix activity 2 screen style

--- feature/activity/src/main/java/com/waleska404/activity/create/SecondActivityScreen.kt
+++ feature/activity/src/main/java/com/waleska404/activity/create/SecondActivityScreen.kt
@@ -5,7 +5,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
@@ -14,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun SecondActivityScreen(
@@ -22,21 +25,43 @@ fun SecondActivityScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.White),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Top
+            .background(Color.White)
+            .padding(26.dp),
     ) {
-        Spacer(modifier = Modifier.height(22.dp))
-        Text(text = "Activity 2")
-        Divider(thickness = 1.dp)
-        Spacer(modifier = Modifier.height(22.dp))
-        Text(text = "State:")
-        Text(text = "Activity 1: CREATED")
-        Text(text = "Activity 2: CREATED")
-        Button(
-            onClick = { onReturnToActivity1Clicked() }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.White),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
         ) {
-            Text(text = "Return to Activity 1")
+            Spacer(modifier = Modifier.height(12.dp))
+            Text(
+                fontSize = 30.sp,
+                text = "Activity 2"
+            )
+            Divider(thickness = 1.dp)
+        }
+        Spacer(modifier = Modifier.height(22.dp))
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.White),
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Top
+        ) {
+            Text(text = "State:")
+            Text(text = "Activity 1: CREATED")
+            Text(text = "Activity 2: CREATED")
+            Spacer(modifier = Modifier.height(22.dp))
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {
+                    onReturnToActivity1Clicked()
+                }
+            ) {
+                Text(text = "Return to Activity 1")
+            }
         }
     }
 }
