7e133a770faad0835152fb57177af547c5061066
Paula Boyano Ivars <paulaboyanoivars@gmail.com>
2023-12-19 21:20:10 +0100

    :lipstick: fix activity screen style

--- feature/activity/src/main/java/com/waleska404/activity/create/FirstActivityScreen.kt
+++ feature/activity/src/main/java/com/waleska404/activity/create/FirstActivityScreen.kt
@@ -5,7 +5,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
@@ -14,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun FirstActivityScreen(
@@ -25,31 +28,52 @@ fun FirstActivityScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.White),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Top
+            .background(Color.White)
+            .padding(26.dp),
     ) {
-        Spacer(modifier = Modifier.height(22.dp))
-        Text(text = "Activity 1")
-        Divider(thickness = 1.dp)
-        Spacer(modifier = Modifier.height(22.dp))
-        Text(text = "State:")
-        Text(text = "Activity 1: CREATED")
-        Text(text = "Activity 2: $activity2Text")
-        Button(
-            onClick = {
-                navigateToActivity2()
-                changeTextToDestroyed()
-            }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.White),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
         ) {
-            Text(text = "Go to Activity 2")
+            Spacer(modifier = Modifier.height(12.dp))
+            Text(
+                fontSize = 30.sp,
+                text = "Activity 1"
+            )
+            Divider(thickness = 1.dp)
         }
-        Button(
-            onClick = {
-                changeTextToNotCreated()
-            }
+        Spacer(modifier = Modifier.height(22.dp))
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.White),
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Top
         ) {
-            Text(text = "RESET")
+            Text(text = "State:")
+            Text(text = "Activity 1: CREATED")
+            Text(text = "Activity 2: $activity2Text")
+            Spacer(modifier = Modifier.height(22.dp))
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {
+                    navigateToActivity2()
+                    changeTextToDestroyed()
+                }
+            ) {
+                Text(text = "Go to Activity 2")
+            }
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {
+                    changeTextToNotCreated()
+                }
+            ) {
+                Text(text = "RESET")
+            }
         }
     }
 }
