470bd3c7ac83a77a939e22ed69e800cfd6a5d8ea
Paula Boyano Ivars <paulaboyanoivars@gmail.com>
2023-12-19 21:03:55 +0100

    :lipstick: fix home screen style

--- feature/home/src/main/java/com/waleska404/home/HomeScreen.kt
+++ feature/home/src/main/java/com/waleska404/home/HomeScreen.kt
@@ -4,29 +4,45 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen(
     navigateToActivityScreen: () -> Unit,
 ) {
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Top
+        verticalArrangement = Arrangement.Top,
     ) {
+        Spacer(modifier = Modifier.height(12.dp))
+        Text(
+            fontSize = 30.sp,
+            text = "Topics"
+        )
         Spacer(modifier = Modifier.height(22.dp))
-        Text(text = "Topics")
         Button(
+            modifier = Modifier.fillMaxWidth(),
             onClick = navigateToActivityScreen
         ) {
             Text(text = "Activity Lifecycle")
+        }
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = {  }
+        ) {
+            Text(text = "Fragment Lifecycle")
         }
     }
 }
