c0b6ab519751f961ec6c184fe13d546163c010fa
Paula Boyano Ivars <paulaboyanoivars@gmail.com>
2023-12-20 16:57:03 +0100

    :lipstick: activity 2 screen style fixed

--- feature/activity/src/main/java/com/waleska404/activity/create/SecondActivityScreen.kt
+++ feature/activity/src/main/java/com/waleska404/activity/create/SecondActivityScreen.kt
@@ -3,20 +3,30 @@ package com.waleska404.activity.create
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.waleska404.ui.R
+import com.waleska404.ui.components.CustomIconButton
 
 @Composable
 fun SecondActivityScreen(
@@ -25,43 +35,105 @@ fun SecondActivityScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.White)
+            .background(MaterialTheme.colorScheme.background)
             .padding(26.dp),
     ) {
         Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .background(Color.White),
+                .fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Top
         ) {
             Spacer(modifier = Modifier.height(12.dp))
             Text(
+                text = stringResource(id = R.string.activity_2_title),
+                fontWeight = FontWeight.Bold,
                 fontSize = 30.sp,
-                text = "Activity 2"
+                color = MaterialTheme.colorScheme.secondary,
+                modifier = Modifier.padding(8.dp),
+                lineHeight = 30.sp,
+                textAlign = TextAlign.Center,
+                maxLines = 2,
             )
-            Divider(thickness = 1.dp)
+            Divider(
+                thickness = 1.dp,
+                color = MaterialTheme.colorScheme.secondary
+            )
         }
         Spacer(modifier = Modifier.height(22.dp))
-        Column(
+        Card(
             modifier = Modifier
-                .fillMaxWidth()
-                .background(Color.White),
-            horizontalAlignment = Alignment.Start,
-            verticalArrangement = Arrangement.Top
+                .wrapContentSize(),
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.secondary
+            )
         ) {
-            Text(text = "State:")
-            Text(text = "Activity 1: CREATED")
-            Text(text = "Activity 2: CREATED")
-            Spacer(modifier = Modifier.height(22.dp))
-            Button(
-                modifier = Modifier.fillMaxWidth(),
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.Transparent)
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.Top
+            ) {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = stringResource(id = R.string.state),
+                    color = MaterialTheme.colorScheme.background,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+                // Activity 1
+                Row(
+                    verticalAlignment = Alignment.Bottom,
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.activity_1_state),
+                        color = MaterialTheme.colorScheme.background
+                    )
+                    Spacer(modifier = Modifier.width(5.dp))
+                    Text(
+                        text = stringResource(id = R.string.activity_1_state_value),
+                        color = MaterialTheme.colorScheme.outline,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+
+                // Activity 2
+                Row(
+                    verticalAlignment = Alignment.Bottom,
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.activity_2_state),
+                        color = MaterialTheme.colorScheme.background
+                    )
+                    Spacer(modifier = Modifier.width(5.dp))
+                    Text(
+                        text = stringResource(id = R.string.activity_1_state_value),
+                        color = MaterialTheme.colorScheme.outline,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(22.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            CustomIconButton(
+                modifier = Modifier.weight(1f),
+                text = stringResource(id = R.string.return_to_activity_1),
                 onClick = {
                     onReturnToActivity1Clicked()
-                }
-            ) {
-                Text(text = "Return to Activity 1")
-            }
+                },
+                iconResource = R.drawable.life,
+                iconDescriptionResource = R.string.life_icon
+            )
         }
     }
 }
