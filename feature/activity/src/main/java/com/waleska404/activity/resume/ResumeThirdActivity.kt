161a28836fd941947c4afd64f73ad7ef7ca8991a
Paula Boyano Ivars <paulaboyanoivars@gmail.com>
2023-12-26 16:14:48 +0100

    :construction: onresume third activity screen

--- feature/activity/src/main/java/com/waleska404/activity/resume/ResumeThirdActivity.kt
+++ feature/activity/src/main/java/com/waleska404/activity/resume/ResumeThirdActivity.kt
@@ -2,11 +2,40 @@ package com.waleska404.activity.resume
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.waleska404.activity.R
+import android.util.Log
+import androidx.activity.compose.setContent
+import com.waleska404.ui.theme.AndroidLifecycleTheme
 
 class ResumeThirdActivity : AppCompatActivity() {
+
+    private val TAG = "MYTAG - Activity3"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_resume_third)
+        Log.d(TAG, "onCreate")
+        setContent {
+            AndroidLifecycleTheme {
+                ResumeThirdActivityScreen(
+                    returnToActivity2 = {
+                        finish()
+                    },
+                )
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
     }
 }
---
+++ feature/activity/src/main/java/com/waleska404/activity/resume/ResumeThirdActivityScreen.kt
@@ -0,0 +1,277 @@
+package com.waleska404.activity.resume
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.waleska404.ui.R
+import com.waleska404.ui.components.CustomIconButton
+import com.waleska404.ui.theme.TransparentBlue
+
+@Composable
+fun ResumeThirdActivityScreen(
+    returnToActivity2: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(TransparentBlue)
+            .padding(16.dp)
+            .verticalScroll(
+                state = rememberScrollState()
+            ),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        HeaderThirdResumeScreen()
+        ThirdResumeScreen()
+        FooterThirdResumeScreen(
+            returnToActivity2 = returnToActivity2
+        )
+    }
+}
+
+
+@Composable
+fun ThirdResumeScreen() {
+    Card(
+        modifier = Modifier
+            .wrapContentSize(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.secondary
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Transparent)
+                .padding(16.dp),
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Top
+        ) {
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = stringResource(id = R.string.callbacks_executed),
+                color = MaterialTheme.colorScheme.background,
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+
+            // Activity 1
+            Row(
+                verticalAlignment = Alignment.Bottom,
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Text(
+                    text = stringResource(id = R.string.activity_1_state),
+                    color = MaterialTheme.colorScheme.background
+                )
+                Spacer(modifier = Modifier.width(5.dp))
+                Text(
+                    text = stringResource(id = R.string.activity_1_state_value),
+                    color = MaterialTheme.colorScheme.outline,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            Row(
+                verticalAlignment = Alignment.Bottom,
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Text(
+                    text = stringResource(id = R.string.activity_1_state),
+                    color = MaterialTheme.colorScheme.background
+                )
+                Spacer(modifier = Modifier.width(5.dp))
+                Text(
+                    text = stringResource(id = R.string.on_start),
+                    color = MaterialTheme.colorScheme.outline,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            // Activity 2
+            Row(
+                verticalAlignment = Alignment.Bottom,
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Text(
+                    text = stringResource(id = R.string.activity_2_state),
+                    color = MaterialTheme.colorScheme.background
+                )
+                Spacer(modifier = Modifier.width(5.dp))
+                Text(
+                    text = stringResource(id = R.string.activity_1_state_value),
+                    color = MaterialTheme.colorScheme.outline,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            Row(
+                verticalAlignment = Alignment.Bottom,
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Text(
+                    text = stringResource(id = R.string.activity_2_state),
+                    color = MaterialTheme.colorScheme.background
+                )
+                Spacer(modifier = Modifier.width(5.dp))
+                Text(
+                    text = stringResource(id = R.string.on_start),
+                    color = MaterialTheme.colorScheme.outline,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            // Activity 1
+            Row(
+                verticalAlignment = Alignment.Bottom,
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Text(
+                    text = stringResource(id = R.string.activity_1_state),
+                    color = MaterialTheme.colorScheme.background
+                )
+                Spacer(modifier = Modifier.width(5.dp))
+                Text(
+                    text = stringResource(id = R.string.on_stop),
+                    color = MaterialTheme.colorScheme.inverseOnSurface,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            // Activity 3
+            Row(
+                verticalAlignment = Alignment.Bottom,
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Text(
+                    text = stringResource(id = R.string.activity_3_state),
+                    color = MaterialTheme.colorScheme.background
+                )
+                Spacer(modifier = Modifier.width(5.dp))
+                Text(
+                    text = stringResource(id = R.string.activity_1_state_value),
+                    color = MaterialTheme.colorScheme.outline,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            // Activity 3
+            Row(
+                verticalAlignment = Alignment.Bottom,
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Text(
+                    text = stringResource(id = R.string.activity_3_state),
+                    color = MaterialTheme.colorScheme.background
+                )
+                Spacer(modifier = Modifier.width(5.dp))
+                Text(
+                    text = stringResource(id = R.string.on_start),
+                    color = MaterialTheme.colorScheme.outline,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            // Activity 2
+            Row(
+                verticalAlignment = Alignment.Top,
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Text(
+                    text = stringResource(id = R.string.activity_2_state),
+                    color = MaterialTheme.colorScheme.background
+                )
+                Spacer(modifier = Modifier.width(5.dp))
+                Text(
+                    text = stringResource(id = R.string.on_stop_not_executed),
+                    color = MaterialTheme.colorScheme.background,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+
+        }
+    }
+}
+
+@Composable
+fun HeaderThirdResumeScreen() {
+    Spacer(modifier = Modifier.height(12.dp))
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = stringResource(id = R.string.activity_3_title),
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp,
+            color = MaterialTheme.colorScheme.secondary,
+            modifier = Modifier.padding(8.dp),
+            lineHeight = 30.sp,
+            textAlign = TextAlign.Center,
+            maxLines = 2,
+        )
+    }
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = stringResource(id = R.string.activity_3_subtitle),
+            fontWeight = FontWeight.Bold,
+            fontSize = 15.sp,
+            color = MaterialTheme.colorScheme.secondary,
+            modifier = Modifier.padding(8.dp),
+            lineHeight = 30.sp,
+            textAlign = TextAlign.Center,
+            maxLines = 2,
+        )
+    }
+    Divider(
+        thickness = 1.dp,
+        color = MaterialTheme.colorScheme.secondary
+    )
+    Spacer(modifier = Modifier.height(22.dp))
+}
+
+@Composable
+fun FooterThirdResumeScreen(
+    returnToActivity2: () -> Unit,
+) {
+    Spacer(modifier = Modifier.height(22.dp))
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        CustomIconButton(
+            modifier = Modifier.weight(1f),
+            text = stringResource(id = R.string.return_to_activity_2),
+            onClick = {
+                returnToActivity2()
+            },
+            iconResource = R.drawable.ret,
+            iconDescriptionResource = R.string.return_icon
+        )
+    }
+}
