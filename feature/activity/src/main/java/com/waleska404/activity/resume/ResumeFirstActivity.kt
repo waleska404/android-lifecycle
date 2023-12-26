fe6757f47a1038ec21fb37e862dea5a5d72cacd4
Paula Boyano Ivars <paulaboyanoivars@gmail.com>
2023-12-26 15:56:23 +0100

    :construction: onresume first activity

---
+++ feature/activity/src/main/java/com/waleska404/activity/resume/ResumeFirstActivity.kt
@@ -0,0 +1,61 @@
+package com.waleska404.activity.resume
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.mutableStateOf
+import com.waleska404.activity.start.StartSecondActivity
+import com.waleska404.ui.theme.AndroidLifecycleTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class ResumeFirstActivity : AppCompatActivity() {
+
+    private val TAG = "MYTAG - Activity1"
+    private val initState = mutableStateOf(true)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate")
+        setContent {
+            AndroidLifecycleTheme {
+                ResumeFirstActivityScreen(
+                    initialState = initState.value,
+                    navigateToActivity2 = {
+                        startActivity(
+                            Intent(this, StartSecondActivity::class.java)
+                        )
+                    },
+                    setInitStateToTrue = {
+                        initState.value = true
+                    },
+                    setInitStateToFalse = {
+                        CoroutineScope(Dispatchers.Main).launch {
+                            delay(1000)
+                            initState.value = false
+                        }
+                    }
+                )
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+}
---
+++ feature/activity/src/main/java/com/waleska404/activity/resume/ResumeFirstActivityScreen.kt
@@ -0,0 +1,13 @@
+package com.waleska404.activity.resume
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ResumeFirstActivityScreen(
+    initialState: Boolean,
+    navigateToActivity2: () -> Unit,
+    setInitStateToTrue: () -> Unit,
+    setInitStateToFalse: () -> Unit
+) {
+
+}
